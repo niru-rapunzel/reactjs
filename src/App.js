29573cb3aa7fd26efbba395a9315d9756dfda6f0
niru manandhar <Rapunzelniru@gmail.com>
2020-08-18 10:41:34 -0600

    initialcommit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter,Route} from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
-function App() {
+
+const App =()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="ui container">
+  
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    
+                        <Route path="/" exact component={StreamList} />
+                        <Route path="/streams/new" exact component={StreamCreate} />
+                        <Route path="/streams/edit" exact component={StreamEdit} />
+                        <Route path="/streams/delete" exact component={StreamDelete} />
+                        <Route path="/streams/show" exact component={StreamShow} />
+                   
+                </div>
+            </BrowserRouter>
+        </div>
+ 
   );
-}
+};
 
 export default App;
+
